653d55e19a00d78993c28b6ecf08b75002b4b38c
horribleinternet <cyberblah@gmail.com>
2025-06-06 01:34:19 +0900

    add the ship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 import pygame
+import player
 
 def main():
     initialized = pygame.init()
@@ -7,12 +8,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.
+    ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0,0,0))
+        ship.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
     
 if __name__ == "__main__":
     main()
