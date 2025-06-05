cd42f7588e932b180f3409f9b5c836ad9810c71f
horribleinternet <cyberblah@gmail.com>
2025-06-06 01:54:09 +0900

    use groups

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updateable, drawable)
     ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0,0,0))
-        ship.update(dt)
-        ship.draw(screen)
+        updateable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
